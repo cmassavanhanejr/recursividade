e50d1c74c304427695eb323427acecf8587bdf54
Carlos Alberto Massavanhane Júnior <33702490+cmassavanhanejr@users.noreply.github.com>
2018-10-04 22:16:13 +0200

    Documentado o codigo
    Documentei o codigo acima de forma a torna-lo claro

--- src/model/NivelDeOcorrencia.java
+++ src/model/NivelDeOcorrencia.java
@@ -1,11 +1,17 @@
 package  model;
 
 /**
- *
+ * Classe que contem um metodo para verificar o nivel de ocorencia de um algarismo num numero
  * @author Arley Bebe
  */
 public class NivelDeOcorrencia {
 
+    /**
+    *Metodo para achar de forma recursiva o numero de vezes que um algarismo ocore em um determinado numero
+    *@param n - numero em causa
+    *@param k - algarismo que queremos verificar ocorencia
+    *@param i - armazena o numero de vezes da ocorencia
+    */
     public static int calcular(int n,int k,int i){
     
         if(n%10==k){
