c80a636011a49f110e2a91fd0202f1649feb5af1
Carlos Alberto Massavanhane Júnior <33702490+cmassavanhanejr@users.noreply.github.com>
2018-10-04 21:32:46 +0200

    Comentado e documentado a classe
    Documentei e comentei esta classe de forma a torna-la clara para futuros programadores que queiram usa-la

--- src/model/Factorial.java
+++ src/model/Factorial.java
@@ -1,15 +1,20 @@
 package model;
 /**
- *
+ * Essa eh uma classe que calcula o factorial de um numero usando a recursividade
  * @author Arley Bebe
  */
 public class Factorial {
     
+    /**
+    * esse eh um metodo que estado que recebe um numero e calcula o seu factorial
+    * @param recursividade
+    */
     public static int factorial(int numero){
-    
+        //verifica o caso base 
         if(numero==1){
             return 1;
         }
+        //caso o baso ainda nao seja verdadeiro ele continua a fazer a multiplicação de forma empilhada
         else{
             return numero*factorial(numero-1);
         }
@@ -17,6 +22,7 @@ public class Factorial {
     }
     
     public static void main(String[] args) {
+        //chama o metodo para calcular factorial e imprime na tela o seu factorial
         System.out.println(factorial(5));
     }
 }
